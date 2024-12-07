794241a0e66d065eeb7cfff02fc70e36da3358c1
Niyati Gupta <niyatigupta1803@gmail.com>
2024-12-08 02:33:53 +0530

    Login works, redirected to events

--- Backend/config/passport.js
+++ Backend/config/passport.js
@@ -1,4 +1,4 @@
-import passport from 'passport';
+import passport from 'passport'; 
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 import User from '../models/User.js';
 import dotenv from 'dotenv';
@@ -32,6 +32,7 @@ async (accessToken, refreshToken, profile, done) => {
   }
 }));
 
+
 passport.serializeUser((user, done) => done(null, user.id));
 passport.deserializeUser(async (id, done) => {
   try {
@@ -41,50 +42,3 @@ passport.deserializeUser(async (id, done) => {
     done(err, null);
   }
 });
-
-// import passport from 'passport';
-// import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
-// import User from '../models/User.js';
-// import dotenv from 'dotenv';
-// dotenv.config({ path: '../.env' });
-
-// passport.use(new GoogleStrategy({
-//   clientID: process.env.GOOGLE_CLIENT_ID,
-//   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//   callbackURL: "http://localhost:5001/auth/google/callback",
-// },
-// async (accessToken, refreshToken, profile, done) => {
-//   try {
-//     // Check if the user already exists
-//     const existingUser = await User.findOne({ googleId: profile.id });
-//     if (existingUser) {
-//       return done(null, existingUser);
-//     }
-
-//     // // If the user doesn't exist, save them to the database
-//     // const newUser = new User({
-//     //   googleId: profile.id,
-//     //   name: profile.displayName,
-//     //   email: profile.emails[0].value,
-//     //   profilePicture: profile.photos[0].value
-//     // });
-//     const newUser = new User({
-//       googleId: profile.id,
-//       name: profile.displayName || 'No Name', // Fallback to 'No Name' if displayName is missing
-//       email: profile.emails?.[0]?.value || 'No Email', // Handle missing email
-//       profilePicture: profile.photos?.[0]?.value || '', // Handle missing profile picture
-//     });
-    
-    
-//     await newUser.save();
-//     return done(null, newUser);
-//   } catch (err) {
-//     done(err, null);
-//   }
-// }));
-
-// passport.serializeUser((user, done) => done(null, user.id));
-// passport.deserializeUser(async (id, done) => {
-//   const user = await User.findById(id);
-//   done(null, user);
-// });
